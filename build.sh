d2a9fc4885d79355c278d2f360396260f2910026
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:15:23 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-install -v -d -m755 "${SHED_FAKEROOT}/usr/share/fonts/dejavu" &&
-install -v -m644 ttf/*.ttf "${SHED_FAKEROOT}/usr/share/fonts/dejavu"
+install -v -d -m755 "${SHED_FAKE_ROOT}/usr/share/fonts/dejavu" &&
+install -v -m644 ttf/*.ttf "${SHED_FAKE_ROOT}/usr/share/fonts/dejavu"
